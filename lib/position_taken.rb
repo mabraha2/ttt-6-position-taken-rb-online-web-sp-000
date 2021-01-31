65b98fb64ec9bb40b27785c88d61d0aaa460181a
Medhanei  Abraha <abrahama2019@gmail.com>
2021-01-31 02:01:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,15 +1,6 @@
 # code your #position_taken? method here!
-def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-board = ["x"," "," "," ","o","o"," "," "," "]
-display_board(board)
-
 def position_taken?(board, index)
-
+  if board[index] == " "
+    return false
+  end
 end
